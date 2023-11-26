c4f3e5eab193fcc76df873409ac439685d838bae
mk360 <hamoudi191@hotmail.fr>
2023-11-27 00:39:01 +0100

    cleanup temp folder after downloads

--- scripts/download-heroes/index.js
+++ scripts/download-heroes/index.js
@@ -14,15 +14,14 @@ const { filename } = cmd(options);
 const fileContents = JSON.parse(fs.readFileSync(path.join(__dirname, filename), "utf-8"));
 
 (async function runHeroPipeline() {
-
-    if (fs.existsSync(path.join(__dirname, "../../temp"))) {
-        fs.rmSync(path.join(__dirname, "../../temp"), { force: true, recursive: true });
-    }
-
     for (let { Name: name } of fileContents) {
         await downloadAssets(name);
         await compileAudio(name);
         const compiledPath = await compileImages(name);
         await finalizeCompiledFile(name, compiledPath);
+    }
+
+    if (fs.existsSync(path.join(__dirname, "../../temp"))) {
+        fs.rmSync(path.join(__dirname, "../../temp"), { force: true, recursive: true });
     }
 })();
