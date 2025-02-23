36779f047b9763f7679bd461cdcd7a3867760dc6
mk360 <36678834+mk360@users.noreply.github.com>
2025-02-23 16:57:12 +0100

    improve reposition animation

--- src/scripts/animations/assists/reposition.ts
+++ src/scripts/animations/assists/reposition.ts
@@ -7,26 +7,30 @@ import toCoords from "../../../utils/to-coords";
 function RepositionAssist(scene: MainScene, assisting: Hero, assisted: Hero, sourceHeroCoordinates: string, targetHeroCoordinates: string) {
     const { x: targetX, y: targetY } = toCoords(sourceHeroCoordinates);
     const { x: sourceX, y: sourceY } = toCoords(targetHeroCoordinates);
-    console.log({ targetX, targetY, sourceX, sourceY });
     const pxTargetCoords = gridToPixels(targetX, targetY);
+    const pxSourceCoords = gridToPixels(sourceX, sourceY);
 
-    const timelineData: Types.Time.TimelineEventConfig[] = [{
-        from: 0,
-        tween: {
-            targets: [assisted],
-            x: pxTargetCoords.x,
-            y: pxTargetCoords.y,
-            duration: 100,
+
+    const timelineData: Types.Time.TimelineEventConfig[] = [
+        {
+            from: 0,
+            tween: {
+                targets: [assisting],
+                x: pxTargetCoords.x,
+                y: pxTargetCoords.y,
+                duration: 100,
+            }
+        },
+        {
+            from: 100,
+            tween: {
+                targets: [assisted],
+                x: pxSourceCoords.x,
+                y: pxSourceCoords.y,
+                duration: 100,
+            }
         }
-    },
-    {
-        from: 0,
-        tween: {
-            targets: [assisting],
-            x: sourceX,
-            y: sourceY
-        }
-    }];
+    ];
 
     return timelineData;
 };
--- src/scripts/animations/move-single-unit.ts
+++ src/scripts/animations/move-single-unit.ts
@@ -5,8 +5,8 @@ import MainScene from "../scenes/mainScene";
 
 function MoveSingleUnit(scene: MainScene, target: Hero, targetCoordinates: { x: number; y: number }) {
     const { x: pxX, y: pxY } = gridToPixels(targetCoordinates.x, targetCoordinates.y);
+    console.log(target.getInternalHero().Name, target.getInternalHero().Position, targetCoordinates)
     target.getInternalHero().Position = [targetCoordinates]; // optimistic assignment while we wait for the server
-
     const timeline = new Time.Timeline(scene, [{
         tween: {
             targets: [target],
